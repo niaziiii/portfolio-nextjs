6f69ea62e0d4d487477daef09d6cf3ccb5ccea06
niaziiii <muhabatkhanx@gmail.com>
2023-06-08 15:38:52 +0500

    about page

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,13 +5,22 @@ import Introduction from "./Introduction";
 import HeroSection from "./HeroSection";
 import MySelf from "./Myself";
 import FindMe from "./Findme";
+import AboutMeIntroduction from "./AboutMe";
+import Specialties from "./Specialties";
 
 export {
+  // layout components
   BackgroundCoverImage,
   NavigationBar,
   Footer,
+
+  // landing page components
   Introduction,
   HeroSection,
   MySelf,
   FindMe,
+
+  // about page components
+  AboutMeIntroduction,
+  Specialties,
 };
