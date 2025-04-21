b8ba94ce863062d2f2f99010c15a1416b3f1f6e2
niaziiii <muhabatkhanx@gmail.com>
2025-04-22 02:51:47 +0500

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
-import React from "react";
+import FindMe from "@/pages/home/FindMe";
+import HeroSection from "@/pages/home/HeroSection";
+import MySelf from "@/pages/home/MySelf";
 
-import { HeroSection, MySelf, FindMe } from "@/components";
 export default function Home() {
   return (
-    <>
+    <div className="">
       <HeroSection />
       <MySelf />
       <FindMe />
-    </>
+    </div>
   );
 }
